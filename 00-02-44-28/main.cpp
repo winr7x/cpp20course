1e983fc114cc8a8d1b0df9919cc261888e5cc870
winr7x <winr7x@gmail.com>
2022-03-08 16:43:33 +0300

    Read full name with spaces

--- 00-02-44-28/main.cpp
+++ 00-02-44-28/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
+    /*
+
     int age1;
     std::string name;
 
@@ -13,6 +16,21 @@ int main(int argc, char **argv)
     std::cin >> name >> age1;
 
     std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
+
+    */
+
+    //Data with spaces
+
+    std::string full_name;
+    int age3;
+    std::cout << "Please type in your full name and age : " << std::endl;
+
+    std::getline(std::cin,full_name);
+
+    std::cin >> age3;
+
+    std::cout << "Hello " << full_name
+              << " you are " << age3 << " years old!" << std::endl;
 
     return 0;
 }
