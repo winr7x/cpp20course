5f39b4cdd6a1109db1e55edc4ab58b3123932337
winr7x <winr7x@gmail.com>
2022-05-17 12:43:55 +0300

    Add 4.7.CharactersAndText

---
+++ 4.VariablesAndDatatypes/4.7.CharactersAndText/main.cpp
@@ -0,0 +1,28 @@
+// https://youtu.be/8jLOx1hD3_o?t=15889
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    char character1 {'a'};
+    char character2 {'r'};
+    char character3 {'r'};
+    char character4 {'o'};
+    char character5 {'w'};
+
+    std::cout << character1 << std::endl;
+    std::cout << character2 << std::endl;
+    std::cout << character3 << std::endl;
+    std::cout << character4 << std::endl;
+    std::cout << character5 << std::endl;
+
+    // One byte in memory : 2^8 = 256 different values (0 ~ 255)
+    std::cout << std::endl;
+
+    char value = 65; // ASCII character code for 'A'
+    std::cout << "value : " << value << std::endl;
+    std::cout << "value(int) : " << static_cast<int>(value) << std::endl;
+
+    return 0;
+}
