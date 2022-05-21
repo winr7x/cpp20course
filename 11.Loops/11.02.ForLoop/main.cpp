1d4b3c564fd40e9a93b237dc6ca9c4fe83ce9643
winr7x <winr7x@gmail.com>
2022-05-21 09:56:23 +0300

    Add 11.02.ForLoop

---
+++ 11.Loops/11.02.ForLoop/main.cpp
@@ -0,0 +1,12 @@
+// https://youtu.be/8jLOx1hD3_o?t=28522
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    for (size_t i{}; i < 10; ++i) { // NEW_FOR_ME Way to initialize i var to zero
+        std::cout << "I love C++" << std::endl;
+    }
+
+    return 0;
+}
