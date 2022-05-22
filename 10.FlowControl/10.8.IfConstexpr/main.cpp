cdc3f8a11867f60e4b93d8ab4283209ebfb9dcdd
winr7x <winr7x@gmail.com>
2022-05-22 09:15:13 +0300

    Add 10.8.IfConstexpr

---
+++ 10.FlowControl/10.8.IfConstexpr/main.cpp
@@ -0,0 +1,16 @@
+// 77. If constexpr
+
+#include <iostream>
+
+
+int main() {
+    const int a = 1;
+    const int b = 2;
+    
+    if constexpr(a < b) { // NEW_FOR_ME Evaluate if at compile time
+        std::cout << "a < b\n";
+    }
+
+    
+    return 0;
+}
