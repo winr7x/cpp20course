3ff84c9bec8e979b090b3005a4843af1ddaae95f
winr7x <winr7x@gmail.com>
2022-05-23 16:04:38 +0300

    Add PowerShell script which checks project doesn't have compiler warning

---
+++ check_no_compiler_warnings.ps1
@@ -0,0 +1,68 @@
+# Checked with PSScriptAnalyzer
+
+param ([ValidateRange(1,999)] [Int] $Jobs)
+
+$ScriptName = $MyInvocation.MyCommand.Name
+
+function Usage {
+  Write-Output "Usage: $ScriptName -Jobs <job_count>"
+}
+
+if ($PSBoundParameters.Count -ne 1) {
+  Usage
+  Exit 1
+}
+
+function Write-Title {
+
+  param (
+    $Title
+  )
+
+  Write-Output ""
+  Write-Output "#######################################################################################################"
+  Write-Output "######### $Title"
+  Write-Output "#######################################################################################################"
+  Write-Output ""
+}
+
+function Rebuild {
+
+  param (
+    $Compiler,
+    $ConfigureExitCode,
+    $BuildExitCode,
+    $Jobs
+  )
+
+  Write-Title $Compiler
+
+  Remove-Item -ErrorAction Ignore -Recurse .\build\
+
+  if ($Compiler -eq "CLANG") {
+    cmake -DROOSTER_WARNINGS_AS_ERRORS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G 'MinGW Makefiles' -S .\ -B .\build\
+  } elseif ($Compiler -eq "GCC") {
+    cmake -DROOSTER_WARNINGS_AS_ERRORS=ON -G 'MinGW Makefiles' -S .\ -B .\build\
+  } elseif ($Compiler -eq "MSVC") {
+    cmake -G 'Visual Studio 17 2022' -S .\ -B .\build\
+  } else {
+    Write-Output "Unexpected compiler $Compiler"
+    Exit 2
+  }
+
+  if(-Not $?) {
+    Write-Output "Cmake configure $Compiler failed";
+    Exit $ConfigureExitCode
+  }
+
+  cmake --build .\build\ --config Release --clean-first -j $Jobs
+
+  if(-Not $?) {
+    Write-Output "Cmake build $Compiler failed";
+    Exit $BuildExitCode
+  }
+}
+
+Rebuild -Compiler CLANG -ConfigureExitCode 3 -BuildExitCode 4 -Jobs $Jobs
+Rebuild GCC -ConfigureExitCode 5 -BuildExitCode 6 -Jobs $Jobs
+Rebuild MSVC -ConfigureExitCode 7 -BuildExitCode 8 -Jobs $Jobs
