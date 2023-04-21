37131c36f8d39868a09330b00446442c4f0bc04f
winr7x <winr7x@gmail.com>
2023-04-21 14:46:14 +0300

    Add 47.09.CustomIteratorsWithViews

---
+++ 47.BuildingIteratorsForCustomContainers/47.09.CustomIteratorsWithViews/main.cpp
@@ -0,0 +1,175 @@
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+#include "boxcontainer.h"
+
+void print(auto view) {
+  for (auto i : view) { // Computation happens here.
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+}
+
+struct Student {
+  friend std::ostream& operator<<(std::ostream& out, const Student& s) {
+    out << "Student [ name : " << s.m_name << ", age : " << s.m_age << "]";
+    return out;
+  }
+  auto operator<=>(const Student& s) const = default;
+  std::string m_name;
+  unsigned int m_age;
+};
+
+// NEW_FOR_ME views is applied to custom container BoxContainer
+
+int main(){
+  BoxContainer<int> vi;
+  vi.add(5);
+  vi.add(1);
+  vi.add(4);
+  vi.add(8);
+  vi.add(5);
+  vi.add(3);
+  vi.add(7);
+  vi.add(9);
+  vi.add(6);
+
+#if 0
+  // std::ranges::filter_view
+  std::cout << std::endl;
+  std::cout << "std::ranges::filter_view : " << std::endl;
+  auto evens = [](int i){
+    return (i %2) == 0;
+  };
+  std::cout << "vi : " ;
+  print(vi);
+  std::ranges::filter_view v_evens = std::ranges::filter_view(vi, evens); // No computation
+  std::cout << "vi evens : ";
+  print(v_evens); // Computation happens in the print function
+  // Print evens on the fly 
+  std::cout << "vi evens : " ;
+  print(std::ranges::filter_view(vi,evens)); // Print even numbers on the fly directly
+#endif
+
+#if 0
+  // std::ranges::transform_view
+  std::cout << std::endl;
+  std::cout << "std::ranges::transform_view : " << std::endl;
+  std::ranges::transform_view v_transformed = std::ranges::transform_view(vi, [] (int i) {
+    return i * 10;
+  });
+  std::cout << "vi : " ;
+  print(vi);
+  std::cout << "vi transformed : ";
+  print(v_transformed);
+  std::cout << "vi : ";
+  print(vi); // Check that container has not been modified
+#endif
+
+#if 1
+  // std::ranges::take_view
+  std::cout <<std::endl;
+  std::cout << "std::ranges::take_view : " << std::endl;
+  std::ranges::take_view v_taken = std::ranges::take_view(vi, 5);
+  std::cout << "vi : " ;
+  print(vi);
+  std::cout << "vi taken : ";
+  print(v_taken);
+
+  // std::ranges::take_while_view
+  std::cout <<std::endl;
+  std::cout << "std::views::take_while : " << std::endl;
+  std::ranges::take_while_view v_taken_while = std::ranges::take_while_view(vi, [](int i) {
+      return i % 2 != 0;
+  });
+  std::cout << "vi : ";
+  print(vi);
+  std::cout << "vi taken_while : ";
+  print(v_taken_while);
+
+  // std::ranges::drop_view : drop n first elements
+  std::cout << std::endl;
+  std::cout << "std::ranges::drop_view : " << std::endl;
+  std::ranges::drop_view v_drop = std::ranges::drop_view(vi, 5);
+  std::cout << "vi : ";
+  print(vi);
+  std::cout << "vi_drop : ";
+  print(v_drop);
+
+  // std::views::drop_while_view : drops elements as long as the predicate is met
+  std::cout << std::endl;
+  std::cout << "std::ranges::drop_while_view : " << std::endl;
+  std::ranges::drop_while_view v_drop_while = std::ranges::drop_while_view(vi, [](int i) {
+      return i % 2 != 0;
+  });
+  std::cout << "vi : ";
+  print(vi);
+  std::cout << "v_drop_while : ";
+  print(v_drop_while);
+#endif
+
+  // Range adaptors
+  // std::views::filter()
+  std::cout << std::endl;
+  std::cout << "std::views::filter : " << std::endl;
+  auto evens1 = [] (int i) {
+      return i % 2  == 0;
+  };
+  std::cout << "vi : " ;
+  print(vi);
+  std::ranges::filter_view v_evens1 = std::views::filter(vi, evens1); // No computation
+  std::cout << "vi evens : ";
+  print(v_evens1); //Computation happens in the print function
+  // Print evens on the fly 
+  std::cout << "vi evens : " ;
+  print(std::views::filter(vi,evens1));
+  // Print odds on the fly
+  std::cout << "vi odds : " ;
+  print(std::views::filter(vi, [] (int i) {
+    return i % 2 != 0;
+  }));
+
+#if 1
+  // Students example
+  std::cout << std::endl;
+  std::cout << "students example : " << std::endl;
+
+  BoxContainer<Student> class_room; //  {{"Mike", 12}, {"John", 17}, {"Drake", 14}, {"Mary", 16}};
+  class_room.add(Student("Mike", 12));
+  class_room.add(Student("John", 17));
+  class_room.add(Student("Drake", 14));
+  class_room.add(Student("Mary", 16));
+  
+  std::cout << std::endl;
+  std::cout << "classroom : " << std::endl;
+  for (auto& s : class_room) {
+    std::cout << "   " <<  s << std::endl;
+  }
+
+  std::ranges::sort(class_room, std::less<>{}, &Student::m_age);
+
+  std::cout << std::endl;
+  std::cout << "classroom (after sort) : " << std::endl;
+  for (auto& s : class_room) {
+    std::cout << "   " <<  s << std::endl;
+  }
+
+
+  std::cout << "\nstudents under 15 : \n";
+  print(std::views::take_while(class_room, [] (const Student& s) {
+    return s.m_age < 15;
+  }));
+
+  auto less_than_15_v = class_room | std::views::take_while([] (const Student& s) {
+    return (s.m_age <15);
+  });
+  std::cout << "\nstudents under 15 : \n";
+  print(less_than_15_v);
+
+  std::cout << "\nEnd!" << std::endl;
+#endif
+
+  return 0;
+}
