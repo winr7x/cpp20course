fe4137a77ad8a7e275a8e17aef7ee4bc3d9bf4a2
winr7x <winr7x@gmail.com>
2023-04-25 19:02:04 +0300

    43.6.ReverseIterators: replace mistakenly commited task solution with necessary code

--- 43.StlContainersAndIterators/43.6.ReverseIterators/main.cpp
+++ 43.StlContainersAndIterators/43.6.ReverseIterators/main.cpp
@@ -1,35 +1,12 @@
-template <typename T>
-std::list<T> union_func(const std::list<T>& l1,
-                        const std::list<T>& l2){
+#include <iostream>
+#include <vector>
 
-    std::list<T> result;
-    auto itl1 = l1.begin();
-    auto itl2 = l2.begin();
-    while( (itl1!=l1.end()) && (itl2!=l2.end())){
+int main(){
 
-        if(*itl1 == *itl2){
-            result.push_back(*itl1);
-            ++itl1;
-            ++itl2;
-        }else if( *itl1 < *itl2){
-            result.push_back(*itl1);
-            ++itl1;
-        }else{
-            result.push_back(*itl2);
-            ++itl2;
-        }
-    }
+    std::vector<int> numbers {1,2,3,4,5,6,7,8,9,10};
 
-    //Handle remaining elements from bigger list
-    while(itl1!=l1.end()){
-        result.push_back(*itl1);
-        ++itl1;
-    }
+    [[maybe_unused]] auto it_rev = numbers.rbegin();
+    // auto notEqual = it_rev != numbers.end(); // UNCOMMENT_TO_SEE error: no match for 'operator!=' (operand types are std::reverse_iterator and std::vector<int>::iterator)
 
-    while(itl2!=l2.end()){
-        result.push_back(*itl2);
-        ++itl2;
-    }
-
-    return result;
+    return 0;
 }
